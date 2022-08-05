f4d57f803baf61ce1ce868bdf6fd84d9e5357320
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-05 13:04:30 +0200

    3 mod 3 autocheck done

--- js/hwjs.js
+++ js/hwjs.js
@@ -744,4 +744,47 @@
 //   // Change code above this line
 // }
 
+/////////////////////////////////////////////// 3 autoproof
+//1
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+// };
+// //2
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     owner: {
+//       name: "Henry",
+//       phone: "982-126-1588",
+//       email: "henry.carter@aptmail.com",
+//     },
+//   };
+  
+  //3
+//   const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//   };
+  
+//   // Change code below this line
+//   const aptRating = apartment.rating;
+//   const aptDescr = apartment.descr;
+//   const aptPrice = apartment.price;
+//   const aptTags = apartment.tags;
+//   console.log (aptRating);
+//   console.log (aptDescr);
+//   console.log (aptPrice);
+//   console.log (aptTags);
+//   // Change code above this line
 
+  //4
