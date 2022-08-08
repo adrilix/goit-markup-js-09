f7b9f5020e68333263b61e7d1165a96f9cf084bc
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-08 13:05:00 +0200

    5

--- js/hwjs.js
+++ js/hwjs.js
@@ -788,3 +788,27 @@
 //   // Change code above this line
 
   //4
+  // const apartment = {
+  //   imgUrl: "https://via.placeholder.com/640x480",
+  //   descr: "Spacious apartment in the city center",
+  //   rating: 4,
+  //   price: 2153,
+  //   tags: ["premium", "promoted", "top"],
+  //   owner: {
+  //     name: "Henry",
+  //     phone: "982-126-1588",
+  //     email: "henry.carter@aptmail.com",
+  //   },
+  // };
+  
+  // // Change code below this line
+  // const ownerName = apartment.owner.name;
+  // const ownerPhone = apartment.owner.phone;
+  // const ownerEmail = apartment.owner.email;
+  // const numberOfTags = apartment.tags.length;
+  // const firstTag = apartment.tags[0];
+  // const lastTag = apartment.tags[2];
+  // // Change code above this line
+
+  //5
+  
