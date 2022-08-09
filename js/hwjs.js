fea76de154b4c12a1de934ea230b9c62016ab86a
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-09 13:04:23 +0200

    3 / 12 done

--- js/hwjs.js
+++ js/hwjs.js
@@ -811,4 +811,185 @@
   // // Change code above this line
 
   //5
+  // const apartment = {
+  //   imgUrl: "https://via.placeholder.com/640x480",
+  //   descr: "Spacious apartment in the city center",
+  //   rating: 4,
+  //   price: 2153,
+  //   tags: ["premium", "promoted", "top"],
+  // };
   
+  // // Change code below this line
+  // const aptRating = apartment['rating'];
+  // const aptDescr = apartment['descr'];
+  // const aptPrice = apartment['price'];
+  // const aptTags = apartment['tags'];
+  // // Change code above this line
+
+  //6
+  // const apartment = {
+  //   imgUrl: "https://via.placeholder.com/640x480",
+  //   descr: "Spacious apartment in the city center",
+  //   rating: 4,
+  //   price: 2153,
+  //   tags: ["premium", "promoted", "top"],
+  //   owner: {
+  //     name: "Henry",
+  //     phone: "982-126-1588",
+  //     email: "henry.carter@aptmail.com",
+  //   },
+  // };
+  // apartment.price = 5000;
+  // apartment.rating = 4.7;
+  // apartment.owner.name ="Henry Sibola";
+  // apartment.tags.push("trusted");
+  
+  // Change code below this line
+
+  //7
+  // const apartment = {
+  //   imgUrl: "https://via.placeholder.com/640x480",
+  //   descr: "Spacious apartment in the city center",
+  //   rating: 4.7,
+  //   price: 5000,
+  //   tags: ["premium", "promoted", "top", "trusted"],
+  //   owner: {
+  //     name: "Henry Sibola",
+  //     phone: "982-126-1588",
+  //     email: "henry.carter@aptmail.com",
+  //   },
+  // };
+  //   apartment.area = 60;
+  //   apartment.rooms= 3;
+  //   apartment.location={
+  //     country : "Jamaica",
+  //     city : "Kingston",
+  //   };
+
+  //8
+//   const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   // Change code below this line
+//   name,
+//   price, 
+//   image,
+//   tags,
+//   // Change code above this line
+// };
+
+//9
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+
+
+//   // Change code above this line
+// };
+
+//10
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment ){
+//   values.push(apartment[key]);
+//   keys.push(key);
+// }
+// console.table(keys);
+// console.table(values);
+
+//11
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)){
+//     // keys.push(key);
+    
+//   values.push(apartment[key]);
+//   };
+//   // Change code above this line
+// }
+
+//12
+
+//13
+
+//14
+
+//15
+
+//16
+
+//17
+
+//18
+
+//19
+
+//20
+
+//21
+
+//22
+
+//23
+
+//24
+
+//25
+
+//26
+
+//27
+
+//28
+
+//29
+
+//30
+
+//30
+
+//31
+
+//32
+
+//33
+
+//34
+
+//35
+
+//36
+
+//37
+
+//38
+
+//39
+
+//40
+
+//41
